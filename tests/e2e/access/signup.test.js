7dabd856b5d6df487faea7e7ef6944e5b50b0524
elbotanist <ahmedelgaidi260@gmail.com>
2023-03-13 23:59:40 +0200

    Finished tests of 'sign up service'.

--- tests/e2e/access/signup.test.js
+++ tests/e2e/access/signup.test.js
@@ -4,7 +4,7 @@ const { faker } = require("@faker-js/faker");
 const { connect, disconnect } = require("../../db.config");
 const app = require("../../../src/server");
 
-const baseURL = "/auth";
+const baseURL = "/auth/sign-up";
 
 beforeAll(async () => {
 	return await connect();
@@ -16,20 +16,113 @@ afterAll(async () => {
 
 const fakeUser = {
 	email: faker.internet.email(),
-	userName: faker.internet.userName("Jeanne"),
+	userName: faker.name.firstName(),
 	password: "teTE!@12",
 	confirmPassword: "teTE!@12",
 };
 
-describe(`"POST "${baseURL}/sign-up"`, () => {
-	it(`1. create new user - should be successful!`, async () => {
+describe(`"POST" ${baseURL} - create new user"`, () => {
+	it(`1. It should create a new user successfully`, async () => {
 		const { status, body } = await request(app)
-			.post(`${baseURL}/sign-up`)
+			.post(baseURL)
 			.send({
 				...fakeUser,
 			});
 
-		console.log(body);
 		expect(status).toBe(200);
+		expect(body.data).toBe(
+			"Please, check your mailbox to verify your email address."
+		);
+	});
+
+	it(`2. Duplicate userName`, async () => {
+		const { status, body } = await request(app)
+			.post(baseURL)
+			.send({
+				...fakeUser,
+			});
+
+		expect(status).toBe(422);
+		expect(body.data[0]).toBe("Sorry, this userName may be already taken!");
+	});
+
+	it("3. Duplicate email", async () => {
+		const { status, body } = await request(app)
+			.post(baseURL)
+			.send({
+				...fakeUser,
+				userName: faker.name.firstName(),
+			});
+
+		expect(status).toBe(422);
+		expect(body.data[0]).toBe("Sorry, this email may be already taken!");
+	});
+
+	it("4. Passwords don't match", async () => {
+		const { status, body } = await request(app).post(baseURL).send({
+			email: faker.internet.email(),
+			userName: faker.name.firstName(),
+			password: "teTE!@12",
+			confirmPassword: "teTE!@13",
+		});
+
+		expect(status).toBe(422);
+		expect(body.data[0]).toBe(
+			`"confirmPassword" field doesn't match "password" field`
+		);
+	});
+
+	it("5. No user inputs provided at all", async () => {
+		const { status, body } = await request(app).post(baseURL);
+
+		expect(status).toBe(422);
+		expect(body.data[0]).toBe('"email" field is required!');
+		expect(body.data[1]).toBe('"userName" field is required!');
+		expect(body.data[2]).toBe('"password" field is required!');
+		expect(body.data[3]).toBe('"confirmPassword" is required');
+	});
+
+	it("6. userName is not found", async () => {
+		const { status, body } = await request(app).post(baseURL).send({
+			email: faker.internet.email(),
+			password: "teTE!@12",
+			confirmPassword: "teTE!@13",
+		});
+
+		expect(status).toBe(422);
+		expect(body.data[0]).toBe('"userName" field is required!');
+	});
+
+	it("7. email is not found", async () => {
+		const { status, body } = await request(app).post(baseURL).send({
+			userName: faker.name.firstName(),
+			password: "teTE!@12",
+			confirmPassword: "teTE!@13",
+		});
+
+		expect(status).toBe(422);
+		expect(body.data[0]).toBe('"email" field is required!');
+	});
+
+	it("8. password is not found", async () => {
+		const { status, body } = await request(app).post(baseURL).send({
+			email: faker.internet.email(),
+			userName: faker.name.firstName(),
+			confirmPassword: "teTE!@13",
+		});
+
+		expect(status).toBe(422);
+		expect(body.data[0]).toBe('"password" field is required!');
+	});
+
+	it("9. confirmPassword is not found", async () => {
+		const { status, body } = await request(app).post(baseURL).send({
+			email: faker.internet.email(),
+			userName: faker.name.firstName(),
+			password: "teTE!@13",
+		});
+
+		expect(status).toBe(422);
+		expect(body.data[0]).toBe('"confirmPassword" is required');
 	});
 });
