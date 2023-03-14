003709f5bd474f1138a7f6abdf79d976243b3b05
elbotanist <ahmedelgaidi260@gmail.com>
2023-03-15 00:47:03 +0200

    Finished tests of "Deactivate user account service".

--- src/middlewares/isVerified.js
+++ src/middlewares/isVerified.js
@@ -3,10 +3,9 @@ const CustomError = require("./../Errors/CustomError");
 
 // TODO: Create new end point to create new verify email address!
 
-// * This middleware needs to be after "protect" middleware to get userId from request!!
+// * Note: This middleware needs to be after "protect" middleware to get userId from request!!
 module.exports = async (req, res, next) => {
 	try {
-		console.log("Hi, from isverified");
 		const user = await User.findOne({ _id: req.userId })
 			.select(
 				`email isVerified isActive isDeleted isOTPEnabled isSMSEnabled isTOTPEnabled isBackupEnabled ${
@@ -16,19 +15,19 @@ module.exports = async (req, res, next) => {
 			.lean();
 
 		if (!user || user.isDeleted) {
-			return res.status(422).json({
+			return res.status(401).json({
 				data: "Sorry, your account is deleted!",
 			});
 		}
 
 		if (!user || !user.isActive) {
-			return res.status(422).json({
+			return res.status(401).json({
 				data: "Sorry, you need to activate your account first!",
 			});
 		}
 
 		if (!user || !user.isVerified) {
-			return res.status(422).json({
+			return res.status(401).json({
 				data: "Sorry, You need to verify your email address first!",
 			});
 		}
