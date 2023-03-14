5ecce5073af9a757c8ca2c59d510de6fe0aed65b
elbotanist <ahmedelgaidi260@gmail.com>
2023-03-14 07:57:51 +0200

    Refactored tests of "verify user email".

--- tests/e2e/access/verify.test.js
+++ tests/e2e/access/verify.test.js
@@ -4,14 +4,10 @@ const { faker } = require("@faker-js/faker");
 const { connect, disconnect } = require("../../db.config");
 const app = require("../../../src/server");
 const User = require("./../../../src/app/Models/User.model");
+const { generate_token } = require("./../../../src/helpers/token");
+const { generate_hash } = require("./../../../src/helpers/hash");
 
 const baseURL = "/auth/verify-email";
-const testVerificationToken = {
-	valid:
-		"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDBmOTQwYjBiMWRjM2JkNGY4YjZkOGQiLCJpYXQiOjE2Nzg3NDI1MzksImV4cCI6MTY3ODgyODkzOX0.Zog3yvD5zoRZfsdqTfktzuMCXs1P_8jMmpSt5L637p5",
-	short: "12",
-	long: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NDBmOTQwYjBiMWRjM2JkNGY4YjZkOGQiLCJpYXQiOjE2Nzg3NDI1MzksImV4cCI6MTY3ODgyODkzOX0.Zog3yvD5zoRZfsdqTfktzuMCXs1P_8jMmpSt5L637p522222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222222",
-};
 
 beforeAll(async () => {
 	return await connect();
@@ -22,12 +18,38 @@ afterAll(async () => {
 });
 
 describe(`"GET" ${baseURL} - Verify user email after sign up"`, () => {
-	it("1. Check verification token presence", async () => {
-		const { status } = await request(app).get(
-			baseURL + `/${testVerificationToken.valid}`
+	it("1. Email should be verified successfully", async () => {
+		// (1) Create and save user into DB
+		const user = await User.create({
+			email: faker.internet.email(),
+			userName: faker.random.alpha(10),
+			isVerified: false,
+			password: await generate_hash("tesTES@!#1232"),
+		});
+
+		// (2) Create verification token
+		const verificationToken = await generate_token({
+			payload: { _id: user._id },
+			secret: process.env.VERIFICATION_TOKEN_SECRET,
+			expiresIn: process.env.VERIFICATION_TOKEN_EXPIRES_IN,
+		});
+
+		// (3) Update user document
+		await User.findByIdAndUpdate(
+			{ _id: user._id },
+			{ $set: { verificationToken } }
 		);
 
-		expect(status).toBe(401);
+		// (4) Now, start our magic!
+		const { status, body } = await request(app).get(
+			`${baseURL}/${verificationToken}`
+		);
+
+		// (5) clean DB
+		await User.findOneAndDelete({ _id: user._id });
+
+		expect(status).toBe(200);
+		expect(body.data).toBe("Your account is verified successfully");
 	});
 
 	it("2. Verification token isn't string", async () => {
@@ -45,9 +67,7 @@ describe(`"GET" ${baseURL} - Verify user email after sign up"`, () => {
 	});
 
 	it("4. Verification token is too short to be true", async () => {
-		const { status, body } = await request(app).get(
-			baseURL + `/${testVerificationToken.short}`
-		);
+		const { status, body } = await request(app).get(baseURL + `/12`);
 
 		expect(status).toBe(422);
 		expect(body.data[0]).toBe(`"verificationToken" param can't be true!`);
@@ -55,7 +75,7 @@ describe(`"GET" ${baseURL} - Verify user email after sign up"`, () => {
 
 	it("5. Verification token is too long to be true", async () => {
 		const { status, body } = await request(app).get(
-			baseURL + `/${testVerificationToken.long}`
+			baseURL + `/${"blabla".repeat(50)}`
 		);
 
 		expect(status).toBe(422);
