3b9fdc663715aa7228caa1e28a7c7367205f582c
elbotanist <ahmedelgaidi260@gmail.com>
2023-04-21 01:59:28 +0200

    Worked on refactoring middlewares and error names

--- src/middlewares/errors/errorHandler.middleware.js
+++ src/middlewares/errors/errorHandler.middleware.js
@@ -1,12 +1,84 @@
-const { failure } = require("../../Exceptions/responseHandler");
+const { httpStatusCodeStrings, httpStatusCodeNumbers } = require("./../../constants/index");
 
 module.exports = (error, req, res, next) => {
-	// TODO: This should handle whatever error happens in our app!
+	// console.log({ name: error.name, code: error.code, message: error.message });
+	switch (error.name) {
+		case "JsonWebTokenError":
+			return res.status(httpStatusCodeNumbers.UNAUTHORIZED).json({
+				success: false,
+				status: httpStatusCodeNumbers.UNAUTHORIZED,
+				code: httpStatusCodeStrings.UNAUTHORIZED,
+				message: `Sorry, the given token is invalid!`,
+			});
 
-	console.log("from Error Handler");
+		case "TokenExpiredError":
+			return res.status(httpStatusCodeNumbers.UNAUTHORIZED).json({
+				success: false,
+				status: httpStatusCodeNumbers.UNAUTHORIZED,
+				code: httpStatusCodeStrings.UNAUTHORIZED,
+				message: `Sorry, the given token is expired!`,
+			});
 
-	if (error) {
-		return failure({ res, error });
+		case "ValidationError":
+			return res.status(httpStatusCodeNumbers.UNPROCESSABLE_ENTITY).json({
+				success: false,
+				status: httpStatusCodeNumbers.UNPROCESSABLE_ENTITY,
+				code: httpStatusCodeStrings.UNPROCESSABLE_ENTITY,
+				message: error.message.split(": ")[2] || error.details.map((error) => error.message),
+			});
+			break;
+
+		case "MongoServerError":
+			console.log({ name: error.name, code: error.code, message: error.message });
+
+			if (error.code === 11000) {
+				return res.status(httpStatusCodeNumbers.UNPROCESSABLE_ENTITY).json({
+					success: false,
+					status: httpStatusCodeNumbers.UNPROCESSABLE_ENTITY,
+					code: httpStatusCodeStrings.UNPROCESSABLE_ENTITY,
+					message: Object.keys(error.keyValue).map(
+						(field) => (field = `Sorry, this ${field} may be already taken!`)
+					)[0],
+				});
+			}
+
+			break;
+
+		case "MongooseError":
+			console.log({ name: error.name, code: error.code, message: error.message });
+
+			if (error.message.includes("timed out")) {
+				return res.status(httpStatusCodeNumbers.REQUEST_TIMEOUT).json({
+					success: false,
+					status: httpStatusCodeNumbers.REQUEST_TIMEOUT,
+					code: httpStatusCodeStrings.REQUEST_TIMEOUT,
+					message: "Sorry, the request is timed out!",
+				});
+			}
+
+			break;
+
+		case "Error":
+			return res.status(error.statusCode).json({
+				success: false,
+				status: error.statuscode,
+				code: error.errorCode,
+				message: error.message,
+			});
+
+		default:
+			// TODO: Remove this after everything is okay!
+			console.log("Unhandled error!!!!!!!!!!!!!!!");
+			console.log("--------------------------------------------");
+			console.log({ name: error.name, code: error.code, message: error.message });
+			console.log("--------------------------------------------");
+
+			return res.status(httpStatusCodeNumbers.INTERNAL_SERVER_ERROR).json({
+				success: false,
+				status: httpStatusCodeNumbers.INTERNAL_SERVER_ERROR,
+				code: httpStatusCodeStrings.INTERNAL_SERVER_ERROR,
+				message: "Sorry, internal server error happened!",
+			});
 	}
 
 	next();
--- src/middlewares/response/responseHandler.middleware.js
+++ src/middlewares/response/responseHandler.middleware.js
@@ -1,8 +1,14 @@
+const { httpStatusCodeStrings, httpStatusCodeNumbers } = require("./../../constants/index");
+
 module.exports = (req, res, next) => {
 	// TODO: Consider logging everything like originalUrl, params, query, body,method, etc...
 	// TODO: (Be careful of injection attacks during this desired implementation!)
 	console.log("from Response Handler");
-	console.log({ route: req });
-	console.log({ result: req.result });
-	res.json({ ...req.result });
+
+	res.status(httpStatusCodeNumbers.OK).json({
+		success: true,
+		status: httpStatusCodeNumbers.OK,
+		code: httpStatusCodeStrings.OK,
+		data: req.result,
+	});
 };
