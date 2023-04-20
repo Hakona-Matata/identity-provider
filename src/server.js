1e2c88baf1ed4ec727a8fd29b5da66bf17640f2e
elbotanist <ahmedelgaidi260@gmail.com>
2023-04-21 01:59:28 +0200

    Worked on refactoring middlewares

--- src/server.js
+++ src/server.js
@@ -7,15 +7,13 @@ require("express-async-errors");
 
 const connect_DB = require("./utils/db.connection.js");
 
-const registerMiddlewares = require("./middlewares/registerMiddlewares.js");
+const applyAllMiddlewares = require("./middlewares/applyAllMiddlewares.js");
 //==================================================
 
 const app = express();
 
-registerMiddlewares(app);
-
 connect_DB(app);
 
-// (1) After intializing our server istance, let's connect to MongoDB!
+applyAllMiddlewares(app);
 
 module.exports = app;
