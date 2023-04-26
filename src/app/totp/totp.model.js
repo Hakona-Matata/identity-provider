b4935869a05a44e8a4ed6b08c05f466026afc41d
elbotanist <ahmedelgaidi260@gmail.com>
2023-04-26 05:44:13 +0200

    Finished documenting "TOTP" module

--- src/app/totp/totp.model.js
+++ src/app/totp/totp.model.js
@@ -2,6 +2,17 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
+/**
+ * TOTPSchema represents the schema of the TOTP collection in MongoDB.
+ * @typedef {Object} TOTPSchema
+ * @property {ObjectId} accountId - The ID of the account associated with this TOTP code.
+ * @property {string} secret - The secret key generated for the TOTP code.
+ * @property {boolean} isTemp - Indicates whether this is a temporary TOTP code or not.
+ * @property {number} failedAttemptCount - The number of failed attempts to verify the TOTP code.
+ * @property {Date} createdAt - The date when this TOTP document was created.
+ * @property {Date} updatedAt - The date when this TOTP document was last updated.
+ */
+
 const TOTPSchema = new Schema(
 	{
 		accountId: {
@@ -16,7 +27,7 @@ const TOTPSchema = new Schema(
 			type: Boolean,
 			default: true,
 		},
-		count: { type: Number, default: 0 }, // number of wrong tries
+		failedAttemptCount: { type: Number, default: 0 }, // number of wrong tries
 	},
 	{
 		timestamps: true,
