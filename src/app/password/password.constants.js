f59069fb5bb39c5dd5361934d8e5a0c85044e5d8
elbotanist <ahmedelgaidi260@gmail.com>
2023-04-26 03:48:57 +0200

    Finished documenting "password" module

--- src/app/password/password.constants.js
+++ src/app/password/password.constants.js
@@ -1,10 +1,23 @@
+/**
+ * A module that contains success and failure messages related to password resetting functionality.
+ * @module PasswordResetMessages
+ * @type {Object}
+ * @property {Object} SUCCESS_MESSAGES - An object containing success messages related to password resetting functionality.
+ * @property {string} SUCCESS_MESSAGES.PASSWORD_CHANGED_SUCCESSFULLY - A message indicating that the password has been changed successfully.
+ * @property {string} SUCCESS_MESSAGES.ACCOUNT_RESET_SUCCESSFULLY - A message indicating that the password has been reset successfully.
+ * @property {string} SUCCESS_MESSAGES.CHECK_MAIL_BOX - A message indicating to the user to check their mailbox.
+ * @property {Object} FAILURE_MESSAGES - An object containing failure messages related to password resetting functionality.
+ * @property {string} FAILURE_MESSAGES.INCORRECT_PASSWORD - A message indicating that the given password is incorrect.
+ * @property {string} FAILURE_MESSAGES.ALREADY_HAVE_VALID_RESET_LINK - A message indicating that the mailbox already has a valid reset link.
+ * @property {string} FAILURE_MESSAGES.ALREADY_RESET_ACCOUNT - A message indicating that the user has already reset their password.
+ */
 module.exports = {
 	SUCCESS_MESSAGES: {
 		PASSWORD_CHANGED_SUCCESSFULLY: "The password is changed successfully!",
 		ACCOUNT_RESET_SUCCESSFULLY: "Password reset successfully!",
 		CHECK_MAIL_BOX: "Please, check your mail box!",
 	},
-	FAILIURE_MESSAGES: {
+	FAILURE_MESSAGES: {
 		INCORRECT_PASSWORD: "Sorry, the given password is incorrect!",
 		ALREADY_HAVE_VALID_RESET_LINK: "Sorry, your mailbox already has a valid reset link!",
 		ALREADY_RESET_ACCOUNT: "Sorry, you already reset your password!",
--- src/app/password/password.controllers.js
+++ src/app/password/password.controllers.js
@@ -1,8 +1,23 @@
+/**
+ * PasswordControllers is a class that handles password-related API requests.
+ * @class
+ */
 const validate = require("../../helpers/validateInput");
-
 const PasswordValidators = require("./password.validators");
 const PasswordServices = require("./password.services");
+
 class PasswordControllers {
+	/**
+	 * Method for changing the password of the logged-in user.
+	 * @async
+	 * @static
+	 * @method
+	 * @param {object} req - The request object.
+	 * @param {object} res - The response object.
+	 * @param {function} next - The next function.
+	 * @throws {BadRequestException} If the input data is invalid.
+	 * @returns {Promise<void>} Nothing.
+	 */
 	static async change(req, res, next) {
 		const passwordData = await validate(PasswordValidators.change, req.body);
 
@@ -15,6 +30,17 @@ class PasswordControllers {
 		next();
 	}
 
+	/**
+	 * Method for requesting a password reset email.
+	 * @async
+	 * @static
+	 * @method
+	 * @param {object} req - The request object.
+	 * @param {object} res - The response object.
+	 * @param {function} next - The next function.
+	 * @throws {BadRequestException} If the input data is invalid.
+	 * @returns {Promise<void>} Nothing.
+	 */
 	static async forget(req, res, next) {
 		const { email } = await validate(PasswordValidators.forget, req.body);
 
@@ -23,6 +49,17 @@ class PasswordControllers {
 		next();
 	}
 
+	/**
+	 * Method for resetting the password of an account using a reset token.
+	 * @async
+	 * @static
+	 * @method
+	 * @param {object} req - The request object.
+	 * @param {object} res - The response object.
+	 * @param {function} next - The next function.
+	 * @throws {BadRequestException} If the input data is invalid.
+	 * @returns {Promise<void>} Nothing.
+	 */
 	static async reset(req, res, next) {
 		const resetAccountData = await validate(PasswordValidators.reset, req.body);
 
