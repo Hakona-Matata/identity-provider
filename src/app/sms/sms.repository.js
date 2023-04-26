8502f17799252f12a38323f921c89a22b4445654
elbotanist <ahmedelgaidi260@gmail.com>
2023-04-26 05:30:17 +0200

    Finished documenting "sms" module

--- src/app/sms/sms.repository.js
+++ src/app/sms/sms.repository.js
@@ -1,3 +1,11 @@
+/**
+ * Repository for OTP over SMS.
+ * @class
+ * @property {Function} insertOne - Inserts a new OTP over SMS document to the database.
+ * @property {Function} findOne - Retrieves a single OTP over SMS document from the database.
+ * @property {Function} updateOne - Updates an OTP over SMS document in the database.
+ * @property {Function} deleteOne - Deletes an OTP over SMS document from the database.
+ */
 const SmsModel = require("./../otp/otp.model");
 
 const { BaseRepository } = require("./../../repository/index");
@@ -7,18 +15,40 @@ class SmsRepository extends BaseRepository {
 		super(SmsModel);
 	}
 
+	/**
+	 * Inserts a new OTP over SMS document to the database.
+	 * @param {object} payload - Payload for creating a new document.
+	 * @returns {object} - Newly created OTP over SMS document.
+	 */
 	async insertOne(payload) {
 		return await super.insertOne({ ...payload, sendingMethod: "SMS" });
 	}
 
+	/**
+	 * Retrieves a single OTP over SMS document from the database.
+	 * @param {object} payload - Payload for finding a document.
+	 * @returns {object} - Retrieved OTP over SMS document.
+	 */
 	async findOne(payload) {
 		return await super.findOne({ ...payload, sendingMethod: "SMS" });
 	}
 
+	/**
+	 * Updates an OTP over SMS document in the database.
+	 * @param {object} filter - Filter for finding the document to update.
+	 * @param {object} setPayload - Payload to set for the update.
+	 * @param {object} unsetPayload - Payload to unset for the update.
+	 * @returns {object} - Result of the update operation.
+	 */
 	async updateOne(filter, setPayload, unsetPayload) {
 		return await super.updateOne({ ...filter, sendingMethod: "SMS" }, setPayload, unsetPayload);
 	}
 
+	/**
+	 * Deletes an OTP over SMS document from the database.
+	 * @param {object} filter - Filter for finding the document to delete.
+	 * @returns {object} - Result of the delete operation.
+	 */
 	async deleteOne(filter) {
 		return await super.deleteOne({ ...filter, sendingMethod: "SMS" });
 	}
--- src/app/sms/sms.validators.js
+++ src/app/sms/sms.validators.js
@@ -1,18 +1,50 @@
+/**
+ * @typedef {Object} JoiObject
+ * @property {Function} validate - Validates the given input value and schema.
+ */
 const Joi = require("joi");
 const { phone, code, id } = require("./../../validators/index");
 
+/**
+ * Joi schema for enabling SMS OTP feature
+ *
+ * @type {JoiObject}
+ */
+const enable = Joi.object({
+	phone,
+});
+
+/**
+ * Joi schema for confirming OTP code
+ *
+ * @type {JoiObject}
+ */
+const confirm = Joi.object({
+	otp: code,
+});
+
+/**
+ * Joi schema for sending SMS OTP
+ *
+ * @type {JoiObject}
+ */
+const send = Joi.object({
+	userId: id,
+});
+
+/**
+ * Joi schema for verifying OTP code
+ *
+ * @type {JoiObject}
+ */
+const verify = Joi.object({
+	accountId: id,
+	otp: code,
+});
+
 module.exports = {
-	enable: Joi.object({
-		phone,
-	}),
-	confirm: Joi.object({
-		otp: code,
-	}),
-	send: Joi.object({
-		userId: id,
-	}),
-	verify: Joi.object({
-		accountId: id,
-		otp: code,
-	}),
+	enable,
+	confirm,
+	send,
+	verify,
 };
